d6df7693279ceebcad4dc841e04fe50b7d439632
mahammad suhel <mahammad.s@gmail.com>
2023-03-31 12:27:15 +0530

    first commit

---
+++ diceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diceApp
+//
+//  Created by Mahammadsuel C on 3/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ diceAppUITests/diceAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  diceAppUITestsLaunchTests.swift
+//  diceAppUITests
+//
+//  Created by Mahammadsuel C on 3/31/23.
+//
+
+import XCTest
+
+final class diceAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
